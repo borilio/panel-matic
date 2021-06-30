426e360284c30e2d5f6b09a76cb8e51557c16d5d
salva <salva@gmail.com>
2021-06-30 14:33:19 +0200

    commit inicial

---
+++ src/app/components/cuenta-atras/cuenta-atras.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cuenta-atras',
+  templateUrl: './cuenta-atras.component.html',
+  styleUrls: ['./cuenta-atras.component.css']
+})
+export class CuentaAtrasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
